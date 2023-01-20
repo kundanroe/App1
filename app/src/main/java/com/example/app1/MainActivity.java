5d4beba812fd546fc188c7e7b2bec6150773a104
kundanroe <kk4971830@gmail.com>
2023-01-20 22:44:14 +0530

    i love you

--- app/src/main/java/com/example/app1/MainActivity.java
+++ app/src/main/java/com/example/app1/MainActivity.java
@@ -13,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //app done
+
 
         Button pButton = (Button) findViewById(R.id.indoChina);
         pButton.setOnClickListener(new View.OnClickListener() {
